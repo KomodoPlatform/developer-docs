e8931468cfaad3bfa82293c8e21a8232fc5c6192
gcharang <mrgcharang@gmail.com>
2018-11-24 19:47:25 +0530

    adds deploy_docker script which enables to deploy while using docker and updates readme

--- README.md
+++ README.md
@@ -71,3 +71,5 @@ You can use Docker to reliably produce a developer environment that won't confli
 * Then do `docker exec -ti komodo_docs /bin/sh` in another terminal to access a terminal inside the container. Now simply follow the instructions detailed in the above `Start Editing` , `Build` sections.
 * To exit from the terminal from the container use the `exit` command.
 * Use `CTRL + C` in the terminal `docker-compose up` has been done to stop the container.
+* To deploy using docker, use the command `./deploy_docker.sh`
+
---
+++ deploy_docker.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+docker build -t komodo_docs_image .
+docker run --rm -v $(pwd):/app/ -w /app/ --entrypoint "yarn" komodo_docs_image "docs:build"
+
+
+
+# navigate into the build output directory
+cd docs/.vuepress/dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+GIT_USER=$(git config --global user.name)
+git push -f git@github.com:$GIT_USER/komodo-docs-vuepress.git master:gh-pages
+
+cd -
