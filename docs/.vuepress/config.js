f3fa28b07a8db4d95eec7b984d9cf319835f9a39
gcharang <mrgcharang@gmail.com>
2019-03-27 11:35:47 +0530

    Resolves Isuue #59; vuepress provides this functionality out of the box

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,24 @@ module.exports = {
   base: "/",
   description: "Documentation for developers building on Komodo",
   themeConfig: {
+    // Assumes GitHub. Can also be a full GitLab url.
+    repo: 'komodoplatform/developer-docs',
+    // Customising the header label
+    // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
+    repoLabel: 'Contribute!',
+
+    // Optional options for generating "Edit this page" link
+
+    // if your docs are in a different repo from your main project:
+    //docsRepo: 'vuejs/vuepress',
+    // if your docs are not at the root of the repo:
+    //docsDir: 'docs',
+    // if your docs are in a specific branch (defaults to 'master'):
+    //docsBranch: 'master',
+    // defaults to false, set to true to enable
+    editLinks: true,
+    // custom text for edit link. Defaults to "Edit this page"
+    editLinkText: 'Help us improve this page!',
     lastUpdated: 'Last Updated',
     nav: [{
         text: "Start Here",
