1b38c43bde6361388156e7113f859b70f6f8dfdc
gcharang <mrgcharang@gmail.com>
2019-03-28 23:09:12 +0530

    removes bsk dir

--- utils/scripts.py
+++ utils/scripts.py
@@ -137,11 +137,11 @@
 #        'vout': 0}], {'address': 0.01})
 
 
-import os
-for filename in os.listdir('../docs/basic-docs/cryptoconditions/'):
-    with open('../docs/basic-docs/cryptoconditions/'+filename, "r") as f, open(filename, 'w+') as g:
-        for currLine in f:
-            if currLine.startswith('**'):
-                g.write('### '+currLine.strip("*\n") + '\n')
-            else:
-                g.write(currLine)
+#import os
+# for filename in os.listdir('../docs/basic-docs/cryptoconditions/'):
+#    with open('../docs/basic-docs/cryptoconditions/'+filename, "r") as f, open(filename, 'w+') as g:
+#        for currLine in f:
+#            if currLine.startswith('**'):
+#                g.write('### '+currLine.strip("*\n") + '\n')
+#            else:
+#                g.write(currLine)
