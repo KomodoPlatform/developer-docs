45ab19a7328c0f40688952715197e32a7f2d32b0
siddhartha-crypto <siddhartha-crypto@protonmail.com>
2019-03-27 18:32:20 -0800

    correct Crypto Conditions => CryptoConditions; remove ref to smart contracts

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -43,7 +43,7 @@ module.exports = {
             ],
             [
               "/basic-docs/start-here/cc-overview.md",
-              "Overview of Crypto Conditions (Smart Contracts)"
+              "Overview of CryptoConditions"
             ]
           ]
         },
@@ -70,12 +70,12 @@ module.exports = {
           ]
         },
         {
-          title: "Crypto Conditions (Smart Contracts)",
+          title: "CryptoConditions",
           collapsable: true,
           children: [
             [
               "/basic-docs/cryptoconditions/cryptoconditions-instructions.md",
-              "Instructions for Crypto Conditions"
+              "Instructions for CryptoConditions"
             ],
             [
                 "/basic-docs/cryptoconditions/cryptoconditions-tutorial.md",
