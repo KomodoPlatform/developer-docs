cb2a4746ca3993dd3cd8f888a157a9aad60bc994
gcharang <mrgcharang@gmail.com>
2019-03-28 20:55:35 +0530

    adds collapse-text component

---
+++ docs/.vuepress/components/collapse-text.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="collapsed">
+    <button @click="toggle()">{{ vHidden ? 'Show' : 'Hide' }}</button><strong> {{ vTitle }}</strong>
+    <div v-show="!vHidden"><slot></slot></div>
+  </div>
+</template>
+
+<style lang="stylus">
+@require '~@default-theme/styles/config.styl' 
+button 
+   display inline-block
+   font-size 0.9em
+   color #fff
+   background-color $accentColor
+   padding 0.4em 0.8em
+   border-radius 4px
+   transition background-color .1s ease
+   box-sizing border-box
+   border-bottom 1px solid darken($accentColor, 10%)
+   &:hover
+     background-color lighten($accentColor, 10%)
+
+</style>
+
+<script>
+  /* 
+   Lisense:
+   https://github.com/feathers-plus/generator-feathers-plus/blob/master/LICENSE
+   The MIT License (MIT)
+   Copyright (c) 2018 Feathers-Plus
+   Copyright (c) 2017-18 John J. Szwaronek. Licensed under the MIT license.
+  
+  
+  
+  Blank lines are required both before and after the <collapse> and </collapse> tags.
+   ...
+   
+   
+   <collapse hidden title="...">
+
+   ...
+
+   </collapse>
+
+   ...
+   */
+  export default {
+    props: ['hidden', 'title'],
+
+    data() {
+      return {
+        vHidden: this.hidden !== undefined,
+        vTitle: this.title
+      };
+    },
+
+    methods: {
+      toggle() {
+        this.vHidden = !this.vHidden;
+      }
+    }
+  }
+</script>
