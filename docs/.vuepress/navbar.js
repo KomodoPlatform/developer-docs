8e6a62af77d5c4366fba182ef828fc071cfe280d
gcharang <mrgcharang@gmail.com>
2019-04-23 16:03:34 +0530

    make nav and side bars variables in config.js

---
+++ docs/.vuepress/navbar.js
@@ -0,0 +1,55 @@
+var navbar = [{
+        text: "Start Here",
+        link: "/basic-docs/start-here/outline-for-new-developers.md"
+    },
+    {
+        text: "Whitepaper",
+        link: "/whitepaper/introduction.md"
+    },
+    {
+        text: "Guides",
+        items: [{
+                text: "Komodo",
+                link: "/komodo/installation.md"
+            },
+            {
+                text: "Asset Chains",
+                link: "/assetchains/beginner-vps-linux.md"
+            },
+            {
+                text: "Notary Nodes",
+                link: "/notary/setup-Komodo-Notary-Node.md"
+            },
+            {
+                text: "Marketmaker V1",
+                link: "/mmV1/introduction.md"
+            }
+        ]
+    },
+    {
+        text: "CC Book",
+        link: "/cc-jl/introduction.md"
+    },
+    {
+        text: "Agama",
+        link: "/gui/agama/introduction.md"
+    },
+    {
+        text: "Resources",
+        items: [{
+                text: "Komodo Platform",
+                link: "/resources/list-all-KomodoPlatform-repos-links.md"
+            },
+            {
+                text: "Third Party",
+                link: "/resources/third-party-repos-resources.md"
+            }
+        ]
+    },
+    {
+        text: "KomodoPlatform.com",
+        link: "https://komodoplatform.com"
+    }
+]
+
+module.exports = navbar;
