687242dd4f47e61d3fe675c84bd96164440d80bb
gcharang <mrgcharang@gmail.com>
2019-05-01 12:24:22 +0530

    makes algolia search box match the theme

--- docs/.vuepress/theme/components/AlgoliaSearchBox.vue
+++ docs/.vuepress/theme/components/AlgoliaSearchBox.vue
@@ -30,8 +30,10 @@ export default {
         docsearch(Object.assign(
           {},
           userOptions,
+          
           {
             inputSelector: '#algolia-search-input',
+            debug: true,
             // #697 Make docsearch work well at i18n mode.
             algoliaOptions: Object.assign({
               'facetFilters': [`lang:${lang}`].concat(algoliaOptions.facetFilters || [])
@@ -60,96 +62,48 @@ export default {
 </script>
 
 <style lang="stylus">
+
+@require '../styles/algolia-styles.styl'
+
 .algolia-search-wrapper
-  & > span
-    vertical-align middle
   .algolia-autocomplete
-    line-height normal
     .ds-dropdown-menu
-      background-color #fff
-      border 1px solid #999
-      border-radius 4px
-      font-size 16px
-      margin 6px 0 0
-      padding 4px
-      text-align left
+      background-color #FFFFFF
+      border 1px solid $borderColor      
       &:before
-        border-color #999
-      [class*=ds-dataset-]
-        border none
-        padding 0
-      .ds-suggestions
-        margin-top 0
+        border-color $borderColor     
       .ds-suggestion
         border-bottom 1px solid $borderColor
+      .ds-dataset-1
+        background-color #2C313B            
     .algolia-docsearch-suggestion--highlight
-      color #2c815b
+      color $accentColor
+    .algolia-docsearch-suggestion--content
+      background-color #2C313B  
     .algolia-docsearch-suggestion
-      border-color $borderColor
-      padding 0
-      .algolia-docsearch-suggestion--category-header
-        padding 5px 10px
-        margin-top 0
-        background $accentColor
+      border-color $borderColor      
+      .algolia-docsearch-suggestion--category-header        
+        background $borderColor
         color #fff
-        font-weight 600
         .algolia-docsearch-suggestion--highlight
-          background rgba(255, 255, 255, 0.6)
-      .algolia-docsearch-suggestion--wrapper
-        padding 0
-      .algolia-docsearch-suggestion--title
-        font-weight 600
-        margin-bottom 0
+          background rgba(255, 255, 255, 0.6)      
+      .algolia-docsearch-suggestion--title        
         color $textColor
       .algolia-docsearch-suggestion--subcategory-column
-        vertical-align top
-        padding 5px 7px 5px 5px
         border-color $borderColor
-        background #f1f3f5
-        &:after
-          display none
+        background #000000        
       .algolia-docsearch-suggestion--subcategory-column-text
-        color #555
+        color #FFF
     .algolia-docsearch-footer
-      border-color $borderColor
+      border-color $borderColor      
+      color $textColor       
     .ds-cursor .algolia-docsearch-suggestion--content
-      background-color #e7edf3 !important
+      background-color #2D6464 !important
       color $textColor
-
-@media (min-width: $MQMobile)
-  .algolia-search-wrapper
-    .algolia-autocomplete
-      .algolia-docsearch-suggestion
-        .algolia-docsearch-suggestion--subcategory-column
-          float none
-          width 150px
-          min-width 150px
-          display table-cell
-        .algolia-docsearch-suggestion--content
-          float none
-          display table-cell
-          width 100%
-          vertical-align top
-        .ds-dropdown-menu
-          min-width 515px !important
 
 @media (max-width: $MQMobile)
   .algolia-search-wrapper
-    .ds-dropdown-menu
-      min-width calc(100vw - 4rem) !important
-      max-width calc(100vw - 4rem) !important
-    .algolia-docsearch-suggestion--wrapper
-      padding 5px 7px 5px 5px !important
     .algolia-docsearch-suggestion--subcategory-column
-      padding 0 !important
-      background white !important
-    .algolia-docsearch-suggestion--subcategory-column-text:after
-      content " > "
-      font-size 10px
-      line-height 14.4px
-      display inline-block
-      width 5px
-      margin -3px 3px 0
-      vertical-align middle
+      background white !important    
 
 </style>
