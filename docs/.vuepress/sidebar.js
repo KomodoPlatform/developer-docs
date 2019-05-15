14a425b1fd17a74b00fa31997543c8c41c5d4d96
siddhartha-crypto <siddhartha-crypto@protonmail.com>
2019-05-15 12:05:47 -0700

    rebuilding project outline

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -3,193 +3,189 @@ var sidebar = [{
     collapsable: true,
     children: [
         [
-            "/basic-docs/komodo-platform-overview/start-here/about-komodo-platform.md",
-            "About The Komodo Platform"
+            "/basic-docs/komodo-platform-overview/start-here/start-here.md",
+            "Start Here"
         ],
         [
-            "/basic-docs/komodo-platform-overview/start-here/overview.md",
-            "Documentation Orientation"
+            "/basic-docs/komodo-platform-overview/educational-outline/educational-outline.md",
+            "Educational Outline"
         ],
         [
-            "/basic-docs/start-here/get-started-smart-chains.md",
-            "Get Started | Smart Chains"
-        ],
-        [
-            "/basic-docs/start-here/get-started-atomicdex.md",
-            "Get Started | AtomicDEX"
+            "/basic-docs/komodo-platform-overview/comprehensive-tutorials/comprehensive-tutorials.md",
+            "Comprehensive Tutorials"
         ],
     ]
   },
-  {
-      title: "Developer Tutorials",
-      collapsable: true,
-      children: [
-          [
-            "/basic-docs/developer-tutorials/tutorial-outline.md",
-            "Tutorial Outline"
-          ],
-          [
-            "/basic-docs/developer-tutorials/create-a-default-smart-chain.md",
-            "Create a Default Smart Chain"
-          ],
-          [
-            "/basic-docs/developer-tutorials/smart-chain-api-basics.md",
-            "Smart Chain API Basics"
-          ],
-          [
-            "/basic-docs/developer-tutorials/fluidity-overview.md",
-            "Fluidity Overview (combine)"
-          ],
-          [
-            "/basic-docs/developer-tutorials/fluidity-instructions.md",
-            "About Fluidity (combine)"
-          ],
-          [
-            "/basic-docs/developer-tutorials/fluidity-conceptual-overview.md",
-            "Fluidity Conceptual Overview (combine)"
-          ],
-            [
-                "/basic-docs/developer-tutorials/introduction-to-atomicdex.md",
-                "Introduction to AtomicDEX"
-            ],
-            [
-                "/basic-docs/atomicdex/atomicdex-walkthrough.md",
-                "AtomicDEX Walkthrough"
-            ],
-      ]
-  },
-  {
-        title: "Smart Chain Setup",
-        collapsable: true,
-        children: [
-            [
-                "/basic-docs/smart-chain-setup/basic-launch-parameters.md",
-                "Maintenance Instructions"
-            ],
-            [
-                "/basic-docs/smart-chain-setup/basic-launch-parameters.md",
-                "Basic Launch Parameters"
-            ],
-            [
-                "/basic-docs/smart-chain-setup/smart-chain-customization-parameters.md",
-                "Customization Parameters"
-            ],
-        ]
-  },
-  {
-    title: "Smart Chain API",
-    collapsable: true,
-    children: [
-      [
-          "/basic-docs/smart-chain-api/address.md",
-          "Address"
-      ],
-      [
-          "/basic-docs/smart-chain-api/blockchain.md",
-          "Blockchain"
-      ],
-      [
-          "/basic-docs/smart-chain-api/cclib.md",
-          "CC Lib"
-      ],
-      [
-          "/basic-docs/smart-chain-api/control.md",
-          "Control"
-      ],
-      [
-          "/basic-docs/smart-chain-api/disclosure.md", 
-          "Disclosure"
-      ],
-      [
-          "/basic-docs/smart-chain-api/generate.md",
-          "Generate"
-      ],
-      [
-          "/basic-docs/smart-chain-api/mining.md",
-          "Mining"
-      ],
-      [
-          "/basic-docs/smart-chain-api/jumblr.md",
-          "Jumblr"
-      ],
-      [
-          "/basic-docs/smart-chain-api/network.md",
-          "Network"
-      ],
-      [
-          "/basic-docs/smart-chain-api/rawtransactions.md",
-          "Raw Transactions"
-      ],
-      [
-          "/basic-docs/smart-chain-api/util.md",
-          "Util"
-      ],
-      [
-          "/basic-docs/smart-chain-api/wallet.md",
-          "Wallet"
-      ]
-    ]
-  },
-  {
-    title: "Fluidity Module API",
-    collapsable: true,
-    children: [
-      [
-          "/basic-docs/customconsensus/channels.md", 
-          "Channels"
-      ],
-      [
-          "/basic-docs/customconsensus/dice.md", 
-          "Dice"
-      ],
-      [
-          "/basic-docs/customconsensus/faucet.md", 
-          "Faucet"
-      ],
-      [
-          "/basic-docs/customconsensus/gateways.md", 
-          "Gateways"
-      ],
-      [
-          "/basic-docs/customconsensus/heir.md", 
-          "Heir"
-      ],
-      [
-          "/basic-docs/customconsensus/musig.md", 
-          "MuSig"
-      ],
-      [
-          "/basic-docs/customconsensus/oracles.md", 
-          "Oracles"
-      ],
-      [
-          "/basic-docs/customconsensus/rewards.md", 
-          "Rewards"
-      ],
-      [
-          "/basic-docs/customconsensus/rogue.md", 
-          "Rogue"
-      ],
-      [
-          "/basic-docs/customconsensus/sudoku.md", 
-          "Sudoku"
-      ],
-      [
-          "/basic-docs/customconsensus/tokens.md", 
-          "Tokens"
-      ]
-    ]
-  },
-  {
-    title: "AtomicDEX API",
-    collapsable: true,
-    children: [
-        [
-            "/basic-docs/atomicdex/atomicdex-api.md", 
-            "AtomicDEX API"
-        ]
-    ]
-  }
+  // {
+      // title: "Developer Tutorials",
+      // collapsable: true,
+      // children: [
+          // [
+            // "/basic-docs/developer-tutorials/tutorial-outline.md",
+            // "Tutorial Outline"
+          // ],
+          // [
+            // "/basic-docs/developer-tutorials/create-a-default-smart-chain.md",
+            // "Create a Default Smart Chain"
+          // ],
+          // [
+            // "/basic-docs/developer-tutorials/smart-chain-api-basics.md",
+            // "Smart Chain API Basics"
+          // ],
+          // [
+            // "/basic-docs/developer-tutorials/fluidity-overview.md",
+            // "Fluidity Overview (combine)"
+          // ],
+          // [
+            // "/basic-docs/developer-tutorials/fluidity-instructions.md",
+            // "About Fluidity (combine)"
+          // ],
+          // [
+            // "/basic-docs/developer-tutorials/fluidity-conceptual-overview.md",
+            // "Fluidity Conceptual Overview (combine)"
+          // ],
+            // [
+                // "/basic-docs/developer-tutorials/introduction-to-atomicdex.md",
+                // "Introduction to AtomicDEX"
+            // ],
+            // [
+                // "/basic-docs/atomicdex/atomicdex-walkthrough.md",
+                // "AtomicDEX Walkthrough"
+            // ],
+      // ]
+  // },
+  // {
+        // title: "Smart Chain Setup",
+        // collapsable: true,
+        // children: [
+            // [
+                // "/basic-docs/smart-chain-setup/basic-launch-parameters.md",
+                // "Maintenance Instructions"
+            // ],
+            // [
+                // "/basic-docs/smart-chain-setup/basic-launch-parameters.md",
+                // "Basic Launch Parameters"
+            // ],
+            // [
+                // "/basic-docs/smart-chain-setup/smart-chain-customization-parameters.md",
+                // "Customization Parameters"
+            // ],
+        // ]
+  // },
+  // {
+    // title: "Smart Chain API",
+    // collapsable: true,
+    // children: [
+      // [
+          // "/basic-docs/smart-chain-api/address.md",
+          // "Address"
+      // ],
+      // [
+          // "/basic-docs/smart-chain-api/blockchain.md",
+          // "Blockchain"
+      // ],
+      // [
+          // "/basic-docs/smart-chain-api/cclib.md",
+          // "CC Lib"
+      // ],
+      // [
+          // "/basic-docs/smart-chain-api/control.md",
+          // "Control"
+      // ],
+      // [
+          // "/basic-docs/smart-chain-api/disclosure.md", 
+          // "Disclosure"
+      // ],
+      // [
+          // "/basic-docs/smart-chain-api/generate.md",
+          // "Generate"
+      // ],
+      // [
+          // "/basic-docs/smart-chain-api/mining.md",
+          // "Mining"
+      // ],
+      // [
+          // "/basic-docs/smart-chain-api/jumblr.md",
+          // "Jumblr"
+      // ],
+      // [
+          // "/basic-docs/smart-chain-api/network.md",
+          // "Network"
+      // ],
+      // [
+          // "/basic-docs/smart-chain-api/rawtransactions.md",
+          // "Raw Transactions"
+      // ],
+      // [
+          // "/basic-docs/smart-chain-api/util.md",
+          // "Util"
+      // ],
+      // [
+          // "/basic-docs/smart-chain-api/wallet.md",
+          // "Wallet"
+      // ]
+    // ]
+  // },
+  // {
+    // title: "Fluidity Module API",
+    // collapsable: true,
+    // children: [
+      // [
+          // "/basic-docs/customconsensus/channels.md", 
+          // "Channels"
+      // ],
+      // [
+          // "/basic-docs/customconsensus/dice.md", 
+          // "Dice"
+      // ],
+      // [
+          // "/basic-docs/customconsensus/faucet.md", 
+          // "Faucet"
+      // ],
+      // [
+          // "/basic-docs/customconsensus/gateways.md", 
+          // "Gateways"
+      // ],
+      // [
+          // "/basic-docs/customconsensus/heir.md", 
+          // "Heir"
+      // ],
+      // [
+          // "/basic-docs/customconsensus/musig.md", 
+          // "MuSig"
+      // ],
+      // [
+          // "/basic-docs/customconsensus/oracles.md", 
+          // "Oracles"
+      // ],
+      // [
+          // "/basic-docs/customconsensus/rewards.md", 
+          // "Rewards"
+      // ],
+      // [
+          // "/basic-docs/customconsensus/rogue.md", 
+          // "Rogue"
+      // ],
+      // [
+          // "/basic-docs/customconsensus/sudoku.md", 
+          // "Sudoku"
+      // ],
+      // [
+          // "/basic-docs/customconsensus/tokens.md", 
+          // "Tokens"
+      // ]
+    // ]
+  // },
+  // {
+    // title: "AtomicDEX API",
+    // collapsable: true,
+    // children: [
+        // [
+            // "/basic-docs/atomicdex/atomicdex-api.md", 
+            // "AtomicDEX API"
+        // ]
+    // ]
+  // }
 ]
 
 module.exports = sidebar;
---
+++ docs/basic-docs/komodo-platform-overview/comprehensive-tutorials/comprehensive-tutorials.md
@@ -0,0 +1,23 @@
+# First Comprehensive Tutorial
+
+We will place comprehensive tutorials here.
+
+## Step One
+
+Step One
+
+## Step Two
+
+Step Two
+
+# Second Comprehensive Tutorial
+
+Second comprehensive tutorial
+
+## Step One
+
+Step One
+
+## Step Two
+
+Step Two
