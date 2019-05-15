7ef7b39a3ddcd5ba9f64b5aca13bcc21d7da9e6f
gcharang <mrgcharang@gmail.com>
2019-05-15 19:22:53 +0530

    updates redirects

--- docs/.vuepress/public/_redirects.js
+++ docs/.vuepress/public/_redirects.js
@@ -1,46 +1,84 @@
 var redirectAliases = {
-  "/basic-docs/cryptoconditions/cc-tokens.html": "/basic-docs/customconsensus/tokens.html",
-  "/basic-docs/cryptoconditions/cc-channels.html": "/basic-docs/customconsensus/channels.html",
-  "/basic-docs/cryptoconditions/cc-momom.html": "/basic-docs/customconsensus/momom.html",
-  "/basic-docs/cryptoconditions/cc-custom.html": "/basic-docs/customconsensus/custom.html",
-  "/basic-docs/cryptoconditions/cc-sudoku.html": "/basic-docs/customconsensus/sudoku.html",
-  "/basic-docs/cryptoconditions/cc-heir.html": "/basic-docs/customconsensus/heir.html",
-  "/basic-docs/cryptoconditions/cc-README.html": "/basic-docs/customconsensus/README.html",
-  "/basic-docs/cryptoconditions/cc-musig.html": "/basic-docs/customconsensus/musig.html",
-  "/basic-docs/cryptoconditions/cryptoconditions-tutorial.html": "/basic-docs/customconsensus/custom-consensus-tutorial.html",
-  "/basic-docs/cryptoconditions/cc-gateways.html": "/basic-docs/customconsensus/gateways.html",
-  "/basic-docs/cryptoconditions/cc-rewards.html": "/basic-docs/customconsensus/rewards.html",
-  "/basic-docs/cryptoconditions/cc-faucet.html": "/basic-docs/customconsensus/faucet.html",
-  "/basic-docs/cryptoconditions/cc-dice.html": "/basic-docs/customconsensus/dice.html",
-  "/basic-docs/cryptoconditions/cc-dilithium.html": "/basic-docs/customconsensus/dilithium.html",
-  "/basic-docs/cryptoconditions/cryptoconditions-instructions.html": "/basic-docs/customconsensus/custom-consensus-instructions.html",
-  "/basic-docs/cryptoconditions/dynamic-modules-instructions.html": "/basic-docs/customconsensus/dynamic-modules-instructions.html",
-  "/basic-docs/cryptoconditions/cc-assets.html": "/basic-docs/customconsensus/assets.html",
-  "/basic-docs/cryptoconditions/cc-oracles.html": "/basic-docs/customconsensus/oracles.html",
-  "/basic-docs/cryptoconditions/cc-rogue.html": "/basic-docs/customconsensus/rogue.html",
-  "/basic-docs/start-here/cc-overview.html": "/basic-docs/start-here/custom-consensus-overview.html",
+  "/basic-docs/cryptoconditions/cc-tokens.html":
+    "/basic-docs/customconsensus/tokens.html",
+  "/basic-docs/cryptoconditions/cc-channels.html":
+    "/basic-docs/customconsensus/channels.html",
+  "/basic-docs/cryptoconditions/cc-momom.html":
+    "/basic-docs/customconsensus/momom.html",
+  "/basic-docs/cryptoconditions/cc-custom.html":
+    "/basic-docs/customconsensus/custom.html",
+  "/basic-docs/cryptoconditions/cc-sudoku.html":
+    "/basic-docs/customconsensus/sudoku.html",
+  "/basic-docs/cryptoconditions/cc-heir.html":
+    "/basic-docs/customconsensus/heir.html",
+  "/basic-docs/cryptoconditions/cc-README.html":
+    "/basic-docs/customconsensus/README.html",
+  "/basic-docs/cryptoconditions/cc-musig.html":
+    "/basic-docs/customconsensus/musig.html",
+  "/basic-docs/cryptoconditions/cryptoconditions-tutorial.html":
+    "/basic-docs/customconsensus/custom-consensus-tutorial.html",
+  "/basic-docs/cryptoconditions/cc-gateways.html":
+    "/basic-docs/customconsensus/gateways.html",
+  "/basic-docs/cryptoconditions/cc-rewards.html":
+    "/basic-docs/customconsensus/rewards.html",
+  "/basic-docs/cryptoconditions/cc-faucet.html":
+    "/basic-docs/customconsensus/faucet.html",
+  "/basic-docs/cryptoconditions/cc-dice.html":
+    "/basic-docs/customconsensus/dice.html",
+  "/basic-docs/cryptoconditions/cc-dilithium.html":
+    "/basic-docs/customconsensus/dilithium.html",
+  "/basic-docs/cryptoconditions/cryptoconditions-instructions.html":
+    "/basic-docs/customconsensus/custom-consensus-instructions.html",
+  "/basic-docs/cryptoconditions/dynamic-modules-instructions.html":
+    "/basic-docs/customconsensus/dynamic-modules-instructions.html",
+  "/basic-docs/cryptoconditions/cc-assets.html":
+    "/basic-docs/customconsensus/assets.html",
+  "/basic-docs/cryptoconditions/cc-oracles.html":
+    "/basic-docs/customconsensus/oracles.html",
+  "/basic-docs/cryptoconditions/cc-rogue.html":
+    "/basic-docs/customconsensus/rogue.html",
+  "/basic-docs/start-here/cc-overview.html":
+    "/basic-docs/start-here/custom-consensus-overview.html",
   "/cc/index-book-jl.html": "/cc-jl/introduction.html",
   "/komodo/install-Komodo-manually.html": "komodo/installation.html",
   "/barterDEX/barterDEX-API.html": "/mmV1/api/introduction.html",
   "/komodo/komodo-API.html": "/basic-docs/komodo-api/address.html",
-  "/agama/add-Bitcoin-Compatible-coin-Agama-Desktop.html": "/gui/agama/desktop/add-Bitcoin-Compatible-coin-Agama-Desktop.html",
-  "/agama/add-ERC20-token-Agama-Desktop.html": "/gui/agama/desktop/add-ERC20-token-Agama-Desktop.html",
-  "/agama/add-Komodo-Assetchains-Agama-Desktop.html": "/gui/agama/desktop/add-Komodo-Assetchains-Agama-Desktop.html",
-  "/agama/mobile/add-Bitcoin-Compatible-coin-Agama-Mobile.html": "/gui/agama/mobile/add-Bitcoin-Compatible-coin-Agama-Mobile.html",
-  "/agama/mobile/add-ERC20-token-Agama-Mobile.html": "/gui/agama/mobile/add-ERC20-token-Agama-Mobile.html",
-  "/agama/mobile/add-Komodo-Assetchains-Agama-Mobile.html": "/gui/agama/mobile/add-Komodo-Assetchains-Agama-Mobile.html",
-  "/general/third-party-repos-resources.html": "/resources/third-party-repos-resources.html",
-  "/general/list-all-KomodoPlatform-Project-links.html": "/resources/list-all-KomodoPlatform-repos-links.html",
-  "/cc/activate-cc-independent-chain.html": "/basic-docs/customconsensus/activate-custom-consensus-assetchain.html",
+  "/agama/add-Bitcoin-Compatible-coin-Agama-Desktop.html":
+    "/gui/agama/desktop/add-Bitcoin-Compatible-coin-Agama-Desktop.html",
+  "/agama/add-ERC20-token-Agama-Desktop.html":
+    "/gui/agama/desktop/add-ERC20-token-Agama-Desktop.html",
+  "/agama/add-Komodo-Assetchains-Agama-Desktop.html":
+    "/gui/agama/desktop/add-Komodo-Assetchains-Agama-Desktop.html",
+  "/agama/mobile/add-Bitcoin-Compatible-coin-Agama-Mobile.html":
+    "/gui/agama/mobile/add-Bitcoin-Compatible-coin-Agama-Mobile.html",
+  "/agama/mobile/add-ERC20-token-Agama-Mobile.html":
+    "/gui/agama/mobile/add-ERC20-token-Agama-Mobile.html",
+  "/agama/mobile/add-Komodo-Assetchains-Agama-Mobile.html":
+    "/gui/agama/mobile/add-Komodo-Assetchains-Agama-Mobile.html",
+  "/general/third-party-repos-resources.html":
+    "/resources/third-party-repos-resources.html",
+  "/general/list-all-KomodoPlatform-Project-links.html":
+    "/resources/list-all-KomodoPlatform-repos-links.html",
+  "/cc/activate-cc-independent-chain.html":
+    "/basic-docs/customconsensus/activate-custom-consensus-assetchain.html",
   "/coins/info.html": "/komodo/info.html",
   "/cc/faq.html": "/cc-jl/faq.html",
+  "/komodo/example-asset-chains.html": "/assetchains/example-asset-chains.html",
   "/barterDEX/trade.html": "/mmV1/usage/trade.html",
-  "/barterDEX/list-of-all-coins-tradable.html": "/mmV1/coin-integration/list-of-all-coins-tradable.html",
-  "/barterDEX/install-barterDEX-CLI.html": "/mmV1/installation/install-marketmakerV1.html",
-  "/barterDEX/add-coin-barterDEX.html": "/mmV1/coin-integration/info-add-coin.html",
-  "/barterDEX/get-listed-barterDEX.html": "/mmV1/coin-integration/coin-integration.html",
-  "/komodo/create-Komodo-Assetchain.html": "/basic-docs/installations/creating-asset-chains.html",
+  "/barterDEX/list-of-all-coins-tradable.html":
+    "/mmV1/coin-integration/list-of-all-coins-tradable.html",
+  "/barterDEX/install-barterDEX-CLI.html":
+    "/mmV1/installation/install-marketmakerV1.html",
+  "/barterDEX/add-coin-barterDEX.html":
+    "/mmV1/coin-integration/info-add-coin.html",
+  "/barterDEX/get-listed-barterDEX.html":
+    "/mmV1/coin-integration/coin-integration.html",
+  "/komodo/create-Komodo-Assetchain.html":
+    "/basic-docs/installations/creating-asset-chains.html",
   "/komodo/checklist-new-coins.": "/assetchains/checklist-new-coin.html",
-  "/komodo/assetchain-params.html": "/basic-docs/installations/asset-chain-parameters.html"
+  "/komodo/assetchain-params.html":
+    "/basic-docs/installations/asset-chain-parameters.html",
+  "/home-cc.html":
+    "/basic-docs/customconsensus/custom-consensus-instructions.html"
 };
 module.exports = redirectAliases;
