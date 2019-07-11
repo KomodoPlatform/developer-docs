66e7a51eaa1745c4b5c2c154fa6d782832094312
gcharang <mrgcharang@gmail.com>
2019-07-11 20:51:00 +0530

    updates old redirects

--- docs/.vuepress/public/_redirects.js
+++ docs/.vuepress/public/_redirects.js
@@ -1,23 +1,22 @@
 var redirectAliases = {
-  "/basic-docs/cryptoconditions/cc-tokens.html": "/basic-docs/customconsensus/tokens.html",
-  "/basic-docs/cryptoconditions/cc-channels.html": "/basic-docs/customconsensus/channels.html",
-  "/basic-docs/cryptoconditions/cc-momom.html": "/basic-docs/customconsensus/momom.html",
-  "/basic-docs/cryptoconditions/cc-custom.html": "/basic-docs/customconsensus/custom.html",
-  "/basic-docs/cryptoconditions/cc-sudoku.html": "/basic-docs/customconsensus/sudoku.html",
-  "/basic-docs/cryptoconditions/cc-heir.html": "/basic-docs/customconsensus/heir.html",
-  "/basic-docs/cryptoconditions/cc-README.html": "/basic-docs/customconsensus/README.html",
-  "/basic-docs/cryptoconditions/cc-musig.html": "/basic-docs/customconsensus/musig.html",
-  "/basic-docs/cryptoconditions/cryptoconditions-tutorial.html": "/basic-docs/customconsensus/custom-consensus-tutorial.html",
-  "/basic-docs/cryptoconditions/cc-gateways.html": "/basic-docs/customconsensus/gateways.html",
-  "/basic-docs/cryptoconditions/cc-rewards.html": "/basic-docs/customconsensus/rewards.html",
-  "/basic-docs/cryptoconditions/cc-faucet.html": "/basic-docs/customconsensus/faucet.html",
-  "/basic-docs/cryptoconditions/cc-dice.html": "/basic-docs/customconsensus/dice.html",
-  "/basic-docs/cryptoconditions/cryptoconditions-instructions.html": "/basic-docs/customconsensus/custom-consensus-instructions.html",
-  "/basic-docs/cryptoconditions/dynamic-modules-instructions.html": "/basic-docs/customconsensus/dynamic-modules-instructions.html",
-  "/basic-docs/cryptoconditions/cc-assets.html": "/basic-docs/customconsensus/assets.html",
-  "/basic-docs/cryptoconditions/cc-oracles.html": "/basic-docs/customconsensus/oracles.html",
-  "/basic-docs/cryptoconditions/cc-rogue.html": "/basic-docs/customconsensus/rogue.html",
-  "/basic-docs/start-here/cc-overview.html":"/basic-docs/start-here/custom-consensus-overview.html" 
+  "/basic-docs/cryptoconditions/cc-tokens.html": "/basic-docs/antara/antara-api/tokens.html",
+  "/basic-docs/cryptoconditions/cc-channels.html": "/basic-docs/antara/antara-api/channels.html",
+  "/basic-docs/cryptoconditions/cc-momom.html": "/basic-docs/smart-chains/smart-chain-api/crosschain.html",
+  "/basic-docs/cryptoconditions/cc-custom.html": "/basic-docs/antara/antara-tutorials/advanced-series-2.html",
+  "/basic-docs/cryptoconditions/cc-sudoku.html": "/basic-docs/antara/antara-api/sudoku.html",
+  "/basic-docs/cryptoconditions/cc-heir.html": "/basic-docs/antara/antara-api/heir.html",
+  "/basic-docs/cryptoconditions/cc-musig.html": "/basic-docs/antara/antara-api/musig.html",
+  "/basic-docs/cryptoconditions/cryptoconditions-tutorial.html": "/basic-docs/antara/antara-tutorials/introduction-to-antara-tutorials.html",
+  "/basic-docs/cryptoconditions/cc-gateways.html": "/basic-docs/antara/antara-api/gateways.html",
+  "/basic-docs/cryptoconditions/cc-rewards.html": "/basic-docs/antara/antara-api/rewards.html",
+  "/basic-docs/cryptoconditions/cc-faucet.html": "/basic-docs/antara/antara-api/faucet.html",
+  "/basic-docs/cryptoconditions/cc-dice.html": "/basic-docs/antara/antara-api/dice.html",
+  "/basic-docs/cryptoconditions/cryptoconditions-instructions.html": "/basic-docs/antara/antara-tutorials/understanding-antara-addresses.html",
+  "/basic-docs/cryptoconditions/dynamic-modules-instructions.html": "/basic-docs/antara/antara-tutorials/understanding-antara-addresses.html",
+  "/basic-docs/cryptoconditions/cc-assets.html": "/basic-docs/antara/antara-api/assets.html",
+  "/basic-docs/cryptoconditions/cc-oracles.html": "/basic-docs/antara/antara-api/oracles.html",
+  "/basic-docs/cryptoconditions/cc-rogue.html": "/basic-docs/antara/antara-api/rogue.html",
+  "/basic-docs/start-here/cc-overview.html": "/basic-docs/antara/introduction-to-antara.html"
+
 };
 module.exports = redirectAliases;
-
