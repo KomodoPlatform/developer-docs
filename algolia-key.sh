bc24f9809c920fddb8529b5cd7f8c77eec09c83f
gcharang <mrgcharang@gmail.com>
2019-07-16 11:50:25 +0530

    updates colors of 404 page and algolia search box

--- algolia-key.sh
+++ algolia-key.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "var algoliaSecret = { key : '$ALGOLIAKEY' }" > ./docs/.vuepress/algolia-secret.js
+echo "var algoliaSecret = { key : \"$ALGOLIAKEY\" }" > ./docs/.vuepress/algolia-secret.js
 echo "module.exports = algoliaSecret;" >> ./docs/.vuepress/algolia-secret.js
