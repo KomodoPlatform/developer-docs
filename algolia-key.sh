5c4060ee9334e2ab4facff8542bd8b7127b79a52
gcharang <mrgcharang@gmail.com>
2019-07-29 20:14:36 +0530

    testing algolia integration

--- algolia-key.sh
+++ algolia-key.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-echo "test"
-echo "var algoliaSecret = { key : \"$ALGOLIAKEY\" }" > ./docs/.vuepress/algolia-secret.js
+echo 'var algoliaSecret = { key : "'$ALGOLIAKEY'" }' > ./docs/.vuepress/algolia-secret.js
 echo "module.exports = algoliaSecret;" >> ./docs/.vuepress/algolia-secret.js
-ls ./docs/.vuepress/
-echo "test1"
+
