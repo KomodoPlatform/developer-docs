1617919a27791b490e329afe8f816e27e300a6a1
gcharang <mrgcharang@gmail.com>
2019-11-22 14:48:05 +0530

    minor edit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,9 +14,7 @@ module.exports = {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PC28587');`
-
-    ],
+})(window,document,'script','dataLayer','GTM-PC28587');`],
     [
       "link",
       {
