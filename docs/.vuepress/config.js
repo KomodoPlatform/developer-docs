2df42be1dd1029ed6bd7832c04f09c5ba1c8bb04
gcharang <mrgcharang@gmail.com>
2020-06-05 20:01:42 +0530

    removes commented out gtm script and disables plain white kmd logo

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -30,15 +30,6 @@ module.exports = {
     },
   },
   head: [
-    /*[
-      "script",
-      {},
-      `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PC28587');`,
-    ],*/
     [
       "link",
       {
@@ -105,7 +96,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       apiKey: algoliaSecret.key,
       indexName: "komodoplatform_doc",
     },
-    logo: "/KMD_Mark_White.png",
+    //logo: "/KMD_Mark_White.png",
     nav: navbarImport,
     sidebar: sidebarExtraImport,
   },
