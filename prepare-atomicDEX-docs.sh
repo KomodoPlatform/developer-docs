5c42e496c6eb729637464160bf6c123255491d09
gcharang <mrgcharang@gmail.com>
2021-11-09 15:27:17 +0530

    Revert "updates atomicdex-docs update script"

--- prepare-atomicDEX-docs.sh
+++ prepare-atomicDEX-docs.sh
@@ -3,9 +3,6 @@
 set -ev
 
 rsync -avh --delete ./docs/basic-docs/atomicdex/ ./AtomicDEX-docs/docs/basic-docs/atomicdex/
-rsync -avh --delete ./docs/basic-docs/atomicdex-api-20/ ./AtomicDEX-docs/docs/basic-docs/atomicdex/atomicdex-api-20/
-rsync -avh --delete ./docs/basic-docs/atomicdex-api-20-dev/ ./AtomicDEX-docs/docs/basic-docs/atomicdex/atomicdex-api-20-dev/
-rsync -avh --delete ./docs/basic-docs/atomicdex-api-legacy/ ./AtomicDEX-docs/docs/basic-docs/atomicdex/atomicdex-api-legacy/
 rsync -avh --delete ./docs/.vuepress/public/ ./AtomicDEX-docs/docs/.vuepress/public/
 rm -f ./AtomicDEX-docs/docs/.vuepress/atomicDEX-sidebar.js
 cp ./docs/.vuepress/atomicDEX-sidebar.js ./AtomicDEX-docs/docs/.vuepress/atomicDEX-sidebar.js
